dc4cdd698f96ad59e69496f8e04c09b1880bfc79
Dimas Mahbiali <dimasmahbialay@gmail.com>
2016-12-05 21:12:08 +0700

    Ohmmeter
    Ohmmeter

---
+++ ohmmeter.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(A0,INPUT);
+int ANALOG = analogRead(A0);
+float tegangan= ANALOG * 5.0 /1023.0;
+float i = (5.0 - tegangan)/2200.0;//nilai resistor yang menjadi acuan(R1)
+float resistor = tegangan /i;
+Serial.println(tegangan);
+Serial.println(resistor);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
